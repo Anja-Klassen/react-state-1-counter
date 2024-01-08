80dd73c0b90dfa4e4e4fa17796b93aa046b29db2
Anja Klaßen <anja.klassen@posteo.de>
2024-01-08 15:46:01 +0100

    add solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let count = 0;
+  //let count = 0;
+  const [count, setCount] = useState(0);
+
+  function handleClickDown() {
+    //console.log("clicked");
+    setCount(count - 1);
+  }
+
+  function handleClickUp() {
+    //console.log("clicked");
+    setCount(count + 1);
+  }
 
   return (
     <div className="container">
@@ -10,17 +22,19 @@ export default function App() {
       <div className="button-container">
         <button
           type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
+          //onClick={() => {
+          //console.log("🤔");
+          //}}
+          onClick={handleClickDown}
         >
           -
         </button>
         <button
           type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
+          //onClick={() => {
+          //console.log("🤔");
+          //}}
+          onClick={handleClickUp}
         >
           +
         </button>
@@ -28,3 +42,27 @@ export default function App() {
     </div>
   );
 }
+
+// # React State: Counter
+
+// In the `./src/App.js` you'll find the basic building blocks of a counter.
+
+// The general idea here is that the counter should count up when the plus button is
+//clicked, and count down when the minus button is clicked.
+
+// Currently, the value `0` is hardcoded and doesn't respond to the buttons being
+//clicked.
+
+// ## Task
+
+// Refactor the code so that it features React state and makes use of it. Declare
+//a state variable and make sure to have the buttons update said state variable.
+
+// You can use the following hints as a guideline:
+
+// - `let` doesn't seem to be the appropriate way to declare a variable in this
+//instance.
+// - Is there a built-in React function you can use instead?
+//   - Make sure to import said function.
+// - At present, the `<button>`s' event handler is logging an emoji to the console.
+//   - Clearly, the event handler should be handling a different type of functionality.
